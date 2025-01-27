430e68616c123f06476b589bcada3074901fd511
Rodrigo de Oliveira Peres <oliveira.dev00@gmail.com>
2025-01-27 16:11:17 -0400

    Atualizando botões

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 
-function clicarAqui(){
+function clicarSobre(){
     if (bio.style.display == 'none'){
         bio.style.display = 'block'
+        corsobre.style.backgroundImage = 'linear-gradient(to bottom, rgb(247, 247, 188), rgba(133, 248, 242, 0.692))'
+        corsobre.style.color = 'black'
+        
     }else{
         bio.style.display = 'none'
+        corsobre.style = 'reset'
+    }
+}
+
+function clicarProjetos(){
+    if (bio.style.display == 'none'){
+        bio.style.display = 'block'
+        corprojetos.style.backgroundImage = 'linear-gradient(to bottom, rgb(247, 247, 188), rgba(133, 248, 242, 0.692))'
+        corprojetos.style.color = 'black'
+        
+    }else{
+        bio.style.display = 'none'
+        corprojetos.style = 'reset'
+    }
+}
+
+function clicarRedes(){
+    if (bio.style.display == 'none'){
+        bio.style.display = 'block'
+        corredes.style.backgroundImage = 'linear-gradient(to bottom, rgb(247, 247, 188), rgba(133, 248, 242, 0.692))'
+        corredes.style.color = 'black'
+        
+    }else{
+        bio.style.display = 'none'
+        corredes.style = 'reset'
     }
 }
