939f8544c391ff08f68d2f112915f639d9664fb6
Rodrigo de Oliveira Peres <oliveira.dev00@gmail.com>
2025-01-27 19:54:50 -0400

    atualizando

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function clicarSobre(){
 }
 
 function clicarProjetos(){
-    if (bio.style.display == 'none'){
-        bio.style.display = 'block'
+    if (pro_conteudo.style.display == 'none'){
+        pro_conteudo.style.display = 'block'
         corprojetos.style.backgroundImage = 'linear-gradient(to bottom, rgb(247, 247, 188), rgba(133, 248, 242, 0.692))'
         corprojetos.style.color = 'black'
         
     }else{
-        bio.style.display = 'none'
+        pro_conteudo.style.display = 'none'
         corprojetos.style = 'reset'
     }
 }
