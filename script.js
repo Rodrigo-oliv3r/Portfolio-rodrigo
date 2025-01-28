2af63fdfbe9e70a900e28da190bef801f72fff29
Rodrigo de Oliveira Peres <oliveira.dev00@gmail.com>
2025-01-28 16:51:32 -0400

    atualizando funcionalidades

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function clicarSobre(){
     if (bio.style.display == 'none'){
         bio.style.display = 'block'
         corsobre.style.backgroundImage = 'linear-gradient(to bottom, rgb(247, 247, 188), rgba(133, 248, 242, 0.692))'
-        corsobre.style.color = 'black'
+        corsobre.style.color = 'block'
         
     }else{
         bio.style.display = 'none'
@@ -15,7 +15,7 @@ function clicarProjetos(){
     if (pro_conteudo.style.display == 'none'){
         pro_conteudo.style.display = 'block'
         corprojetos.style.backgroundImage = 'linear-gradient(to bottom, rgb(247, 247, 188), rgba(133, 248, 242, 0.692))'
-        corprojetos.style.color = 'black'
+        corprojetos.style.color = 'block'
         
     }else{
         pro_conteudo.style.display = 'none'
@@ -24,13 +24,17 @@ function clicarProjetos(){
 }
 
 function clicarRedes(){
-    if (bio.style.display == 'none'){
-        bio.style.display = 'block'
+    if (redes_sociais.style.display == 'none'){
+        redes_sociais.style.display = 'flex'
         corredes.style.backgroundImage = 'linear-gradient(to bottom, rgb(247, 247, 188), rgba(133, 248, 242, 0.692))'
-        corredes.style.color = 'black'
+        corredes.style.color = 'block'
         
     }else{
-        bio.style.display = 'none'
+        redes_sociais.style.display = 'none'
         corredes.style = 'reset'
     }
+}
+
+function redeStyle(){
+    estilo.style.display = 'block'
 }
